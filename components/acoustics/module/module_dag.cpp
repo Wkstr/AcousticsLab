7ec6a0abc013ac2fb8922714062a60079bd63e4d
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2025-07-30 06:42:11 +0000

    feat: module graph

---
+++ components/acoustics/module/module_dag.cpp
@@ -0,0 +1,32 @@
+#include "module_dag.hpp"
+
+namespace module {
+
+core::Status MDAGBuilderRegistry::registerDAGBuilder(std::string_view name, DAGBuilder builder) noexcept
+{
+    if (name.empty()) [[unlikely]]
+    {
+        LOG(ERROR, "DAG name cannot be empty");
+        return STATUS(EINVAL, "DAG name cannot be empty");
+    }
+    if (!builder) [[unlikely]]
+    {
+        LOG(ERROR, "DAG builder cannot be null");
+        return STATUS(EINVAL, "DAG builder cannot be null");
+    }
+
+    auto it = _dags.find(name);
+    if (it != _dags.end()) [[unlikely]]
+    {
+        LOG(WARNING, "DAG builder for %s already exists", name.data());
+        return STATUS(EEXIST, "DAG builder already exists");
+    }
+
+    _dags.emplace(name, std::move(builder));
+
+    return STATUS_OK();
+}
+
+MDAGBuilderRegistry::DAGBuilderMap MDAGBuilderRegistry::_dags;
+
+} // namespace module
---
+++ components/acoustics/module/module_node.cpp
@@ -0,0 +1,33 @@
+#include "module_node.hpp"
+
+namespace module {
+
+core::Status MNodeBuilderRegistry::registerNodeBuilder(std::string_view name, NodeBuilder builder,
+    bool replace = false) noexcept
+{
+    if (name.empty() || !builder) [[unlikely]]
+    {
+        LOG(ERROR, "Invalid node name or builder function");
+        return STATUS(EINVAL, "Node name or builder function is invalid");
+    }
+
+    auto it = _nodes.find(name);
+    if (it != _nodes.end()) [[unlikely]]
+    {
+        if (!replace)
+        {
+            return STATUS(EEXIST, "Node with this name already exists");
+        }
+        it->second = std::move(builder);
+    }
+    else
+    {
+        _nodes.emplace(name, std::move(builder));
+    }
+
+    return STATUS_OK();
+}
+
+MNodeBuilderRegistry::NodeBuilderMap MNodeBuilderRegistry::_nodes = {};
+
+} // namespace module
