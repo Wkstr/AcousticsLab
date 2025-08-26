8437b1347fce281b7d31b7eb0fbeb0cdcb2a871a
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2025-08-26 09:56:49 +0000

    feat(wip): api v1

---
+++ components/acoustics/api/v1/context.cpp
@@ -0,0 +1,105 @@
+#include "common.hpp"
+
+#include "cmd_break.hpp"
+#include "cmd_info.hpp"
+#include "cmd_rst.hpp"
+#include "cmd_ver.hpp"
+
+#include "api/context.hpp"
+
+#include "core/logger.hpp"
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <string_view>
+#include <utility>
+
+namespace v1 {
+
+static bool initContext()
+{
+    static auto serializer = core::Serializer::create('\r');
+    if (!serializer) [[unlikely]]
+    {
+        LOG(ERROR, "Failed to create default serializer");
+        return false;
+    }
+    v1::defaults::serializer = serializer.get();
+
+    return true;
+}
+
+static bool preInitHooks()
+{
+    return true;
+}
+
+static bool registerCommands()
+{
+    [[maybe_unused]] static auto break_cmd = v1::CmdBreak();
+    [[maybe_unused]] static auto rst_cmd = v1::CmdRst();
+    [[maybe_unused]] static auto ver_cmd = v1::CmdVer();
+    [[maybe_unused]] static auto info_cmd = v1::CmdInfo();
+
+    return true;
+}
+
+} // namespace v1
+
+namespace api {
+
+core::Status Context::report(core::Status status) noexcept
+{
+    auto transport = _console_ptr;
+    if (!transport) [[unlikely]]
+    {
+        LOG(ERROR, "Console transport is not initialized");
+        return status;
+    }
+
+    static size_t counter = 0;
+
+    auto writer = v1::defaults::serializer->writer(v1::defaults::wait_callback,
+        std::bind(&v1::defaults::write_callback, std::ref(*transport), std::placeholders::_1, std::placeholders::_2),
+        std::bind(&v1::defaults::flush_callback, std::ref(*transport)));
+
+    writer["type"] += v1::ResponseType::System;
+    writer["name"] += "SYSTEM";
+    writer["tag"] += std::to_string(counter++);
+    writer["code"] += status.code();
+    writer["msg"] << status.message();
+    {
+        auto data = writer["data"].writer<core::ObjectWriter>();
+        data["ts"]
+            += std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch())
+                   .count();
+    }
+
+    return STATUS_OK();
+}
+
+Context *Context::create() noexcept
+{
+    static Context instance(CONTEXT_VERSION, CONTEXT_CONSOLE, nullptr);
+
+    if (!v1::initContext())
+    {
+        return nullptr;
+    }
+
+    if (!v1::preInitHooks())
+    {
+        return nullptr;
+    }
+
+    if (!v1::registerCommands())
+    {
+        return nullptr;
+    }
+
+    return &instance;
+}
+
+} // namespace api
